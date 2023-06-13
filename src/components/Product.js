a7c6e4b4cd8e49718fe4255c6849d2f7a94aa543
ineschaouachi <ines.chaouachi@esprit.tn>
2023-06-13 12:42:42 +0100

    Last commit

---
+++ src/components/Product.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react'
+import styled from "styled-components"
+export default class Product extends Component {
+
+  render(){
+    
+    return (
+        <ProductFrame>
+          <ProductImageWrapper>
+          <ProductImage src={this.props.product.img}></ProductImage>
+          </ProductImageWrapper>
+          <ProductInfoWrapper>
+            <span>{this.props.product.name}</span>
+            <span>{this.props.product.price}</span>
+          </ProductInfoWrapper>
+        </ProductFrame>
+    );
+  }
+}
+const ProductFrame = styled.div`
+  border-radius : 25px;
+  min-height : 150px;
+  min-width : 150px;
+  background-color : rgb(110,110,100,0.7);
+  margin : 10px;
+  display : flex;
+  flex-direction : column;`;
+
+  const ProductImage = styled.img`
+  width : 100%;
+  height : 100%;
+  border-radius : 25px;`;
+
+  const ProductInfoWrapper = styled.div`
+  margin-top : auto;
+  margin-bottom : 5px;
+  display : flex;
+  flex-direction : column;
+  & > span {
+    text-align : center;
+  }`;
+  const ProductImageWrapper = styled.div`
+  margin-top : 10px;
+  
+  border-radius : 5px;
+  max-width :300px;
+  max-height :300px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+  flex-direction : column;
+  & > span {
+    text-align : center;
+  }`;
+
+
